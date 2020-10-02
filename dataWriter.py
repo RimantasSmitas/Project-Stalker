577e4494c958a497c441ce0c85404ee1c6d47bb7
Rimantas <rimantas.smitas@gmail.com>
2020-10-02 16:29:09 +0200

    Added files to contain setup parameters

--- dataWriter.py
+++ dataWriter.py
@@ -18,3 +18,20 @@ bikes = "{0}{1}{2}{3}{4} {5}\n"
 f.write(peeps.format(year,month,day,hour,minute,cyclist))
 
 f.close()
+###########
+#### Reading the settings from a file ####
+###########
+### the format in the file will as follows theres going a setting per line
+### first line coordinates of the counting line
+### second line frame rate and resolution setting a number from 1 to 3
+### third line DEV EUI
+### fourth line APP EUI
+### fifth line APP KEY
+### For the python side of the software we need to read just the first two lines the rest are for C
+s = open("settings.txt","r+")
+coordinates = (s.readline())
+recordingSetting = (s.readline())
+s.close()
+
+print(coordinates)
+print(recordingSetting)
