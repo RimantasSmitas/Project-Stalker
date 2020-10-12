9cef10905c9bff41e1967a1104379ac264538052
Rimantas <rimantas.smitas@gmail.com>
2020-10-12 23:01:30 +0200

    Added code to convert hexadecimal to decimal

--- otaa/main.c
+++ otaa/main.c
@@ -86,8 +86,9 @@ static void initfunc (osjob_t* j) {
 
 // application entry point
 int main () {
-    osjob_t initjob;
+
     printf("s%",DEVEUI);
+    osjob_t initjob;
     // initialize runtime env
     os_init();
     // initialize debug library
